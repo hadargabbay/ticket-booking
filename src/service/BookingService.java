ace053d73d93b3dc27ce4c93588a71526d68792b
AssafDali <Assaf1997@gmail.com>
2025-12-25 12:12:36 +0200

    Add files via upload

---
+++ src/service/BookingService.java
@@ -0,0 +1,44 @@
+package service;
+
+import dao.IDao;
+import algorithms.IAlgoBooking;
+import models.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class BookingService {
+    private IDao dao;
+    private IAlgoBooking algorithm;
+
+    public BookingService(IDao dao, IAlgoBooking algorithm) { // constructor
+        this.dao = dao;
+        this.algorithm = algorithm;
+    }
+
+    public void setAlgorithm(IAlgoBooking algorithm) { // change the algo based on what we need
+
+        this.algorithm = algorithm;
+    }
+
+    public Ticket bookBestSeat(String showTitle, int customerId) {
+        Show show = dao.getShowByTitle(showTitle);
+        Customer customer = dao.getCustomer(customerId);
+        if (show == null || customer == null) {
+            return null;
+        }
+        Seat bestSeat = algorithm.findBestSeat(show.getSeats());
+
+        if (bestSeat != null) {
+            bestSeat.setAvailable(false);
+
+            int ticketId = dao.getAllTickets().size() + 1;
+            Ticket newTicket = new Ticket(ticketId, customer, show, bestSeat,
+                    java.time.LocalDateTime.now(), bestSeat.getPrice(), "CONFIRMED");
+
+            dao.updateShow(show);
+            dao.saveTicket(newTicket);
+            return newTicket;
+        }
+        return null;
+    }
+}
